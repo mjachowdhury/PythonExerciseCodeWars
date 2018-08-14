ce8cdf8fcbb06fe47f7b828c76bc999a587067b8
Mohammed <study.computing2016@gmail.com>
2018-08-15 00:32:07 +0100

    Count bits
    this program will find out from integer number how many bits

---
+++ countBits.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 00:02:57 2018
+
+@author: Mohammed
+"""
+
+# Function to get no of set bits in binary
+# representation of positive integer n */
+"""
+def countBits(n):
+    count = 0
+    while (n):
+        count += n & 1
+        n >>=1
+    return count
+
+#function calling     
+i = 9
+print(countBits(i))
+"""
+
+
+# Python3 implementation of recursive
+# approach to find the number of set
+# bits in binary representation of 
+# positive integer n
+
+"""
+def countBits(n):
+    #base case
+    if (n == 0):
+        return 0
+    else:
+        #if last bit set add 1 else
+        # add 0
+        return (n & 1) + countBits( n >> 1)
+
+# get value from user
+# Function calling
+n = 9
+print(countBits(n))
+
+"""
+
+# Another way of getting bits ..
+
+def countSetBits(n):
+    return bin(n).count("1")
+
+n = 9
+print(countSetBits(n))
+
