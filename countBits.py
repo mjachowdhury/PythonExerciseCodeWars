ded6b58dfc96a91b6248db80265527912c2ac470
Mohammed <study.computing2016@gmail.com>
2018-08-15 00:34:05 +0100

    Added problem statement

--- countBits.py
+++ countBits.py
@@ -4,6 +4,12 @@ Created on Wed Aug 15 00:02:57 2018
 
 @author: Mohammed
 """
+"""
+Write a function that takes an (unsigned) integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+ 
+"""
 
 # Function to get no of set bits in binary
 # representation of positive integer n */
